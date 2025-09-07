aa1a0b4203948805e5449ad3bfc48a873937b60d
Marisol-Mancera <marisolmancera10@gmail.com>
2025-09-07 18:30:42 +0200

    feat(builder): define HouseBuilder interface

--- src/main/java/dev/marisol/builder_practice/builder/HouseBuilder.java
+++ src/main/java/dev/marisol/builder_practice/builder/HouseBuilder.java
@@ -1,6 +1,25 @@
 package dev.marisol.builder_practice.builder;
 
+import dev.marisol.builder_practice.entity.House;
+
 public interface HouseBuilder {
 
+    //pone todos los atributos en valores iniciales
+    //para poder reutilizar el builder
+    HouseBuilder reset();
+
+    HouseBuilder withRooms(int rooms);
+
+    HouseBuilder withFloors(int floors);
+
+    HouseBuilder withGarage();
+
+    HouseBuilder withGarden();
+
+    HouseBuilder withSwimmingPool();
+
+    HouseBuilder withFancyStatues();
+
+    House build();
     
 } 
